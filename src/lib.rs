4056e0699e739c51b8d8a2b5f4a04a1870592b75
GennaroBiondi <throwawayishere@ultrakill.com>
2026-06-01 16:42:25 +0200

    Initial Commit

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+mod unix_timestamp;
+pub use unix_timestamp::UnixTimestamp;
+
+#[cfg(test)]
+mod tests;
---
+++ src/unix_timestamp.rs
@@ -0,0 +1,20 @@
+use chrono::{TimeZone, Utc};
+use std::time::{SystemTime, UNIX_EPOCH};
+
+pub struct UnixTimestamp(pub i64);
+
+impl UnixTimestamp {
+    pub fn now() -> Self {
+        let secs = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs() as i64;
+
+        UnixTimestamp(secs)
+    }
+
+    pub fn prettify(&self) -> String {
+        let dt = Utc.timestamp_opt(self.0, 0).single().unwrap();
+        dt.to_rfc3339()
+    }
+}
