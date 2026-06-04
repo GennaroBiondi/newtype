059be27f2aec2c8542ae1e3291ffee3c6b4b7d28
GennaroBiondi <throwawayishere@ultrakill.com>
2026-06-04 15:43:52 +0200

    feat(UnixTimestamp): add operator support

--- src/unix_timestamp.rs
+++ src/unix_timestamp.rs
@@ -1,8 +1,31 @@
 use chrono::{TimeZone, Utc};
+use std::ops::{Add, Div, Mul, Sub};
 use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Debug)]
 pub struct UnixTimestamp(pub i64);
+
+macro_rules! impl_op {
+    ($trait:ident, $method:ident, $op:tt) => {
+        impl $trait for UnixTimestamp {
+            type Output = UnixTimestamp;
+            fn $method(self, rhs: Self) -> Self::Output {
+                Self(self.0 $op rhs.0)
+            }
+        }
+        impl $trait for &UnixTimestamp {
+            type Output = UnixTimestamp;
+            fn $method(self, rhs: Self) -> Self::Output {
+                UnixTimestamp(self.0 $op rhs.0)
+            }
+        }
+    };
+}
+
+impl_op!(Add, add, +);
+impl_op!(Sub, sub, -);
+impl_op!(Mul, mul, *);
+impl_op!(Div, div, /);
 
 impl UnixTimestamp {
     pub fn now() -> Self {
@@ -16,11 +39,14 @@ impl UnixTimestamp {
 
     pub fn prettify(&self) -> String {
         let dt = Utc.timestamp_opt(self.0, 0).single().unwrap();
-        dt.to_rfc3339()
+        dt.to_rfc2822()
     }
 
-    pub fn is_due(&self) -> bool {
-        let now = UnixTimestamp::now();
-        self.0 <= now.0
+    pub fn from_ago(&self) -> UnixTimestamp {
+        &Self::now() - self
+    }
+
+    pub fn passed_from(&self, from: Self) -> bool {
+        self.0 <= from.0
     }
 }
